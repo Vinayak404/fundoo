f8a6d56a4291e650985501888ad62302fccc60d9
Vinayak <vinayakavastrd@gmail.com>
2019-11-30 15:42:06 +0530

    [Vinayak] refactor. refactored by eliminating the error in API's in router/userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -5,5 +5,5 @@ const router = express.Router();
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/forgotPassword', userController.forgotPassword);
-router.post('/resetPassword', verifyToken.verify, userController.resetPassword);
-module.exports = router
+router.post('/resetPassword/:token', verifyToken.verify, userController.resetPassword);
+module.exports = router;
