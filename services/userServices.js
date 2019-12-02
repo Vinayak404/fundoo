2cb0e8ed048fbf2140028ef2b79dc10a5cab8e86
Vinayak <vinayakavastrd@gmail.com>
2019-12-02 10:50:19 +0530

    [Vinayak] refactor. moved the implementation part from model to services services/userServices

--- services/userServices.js
+++ services/userServices.js
@@ -1,19 +1,56 @@
 const model = require('../model/userModel')
+const bcrypt = require('bcrypt')
+
 exports.register = (req, callback) => {
     try {
-        model.register(req, (err, data) => {
-            if (err) callback(err);
-            else callback(null, data)
+        model.user.findOne({
+            email: req.body.email
+        }, (err, data) => {
+            if (data) {
+                callback("user already exists")
+            } else {
+                if (err) {
+                    console.log('failed at model backend', err);
+
+                } else {
+                    bcrypt.hash(req.body.password, 10, (err, encrypted) => {
+                        let user1 = new model.user({
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            email: req.body.email,
+                            password: encrypted
+                        })
+                        user1.save((err, data) => {
+                            if (err) {
+                                console.log('failed to save to db', err);
+                                callback(err)
+                            } else {
+                                console.log('saved in the database', data);
+                                callback(null, data);
+                            }
+                        })
+                    })
+                }
+            }
         })
+
     } catch (e) {
         console.log(e);
     }
 }
 exports.login = (req, callback) => {
     try {
-        model.login(req, (err, data) => {
-            if (err) callback(err);
-            else callback(null, data)
+        model.user.findOne({
+            email: req.body.email
+        }, (err, data) => {
+            if (data) {
+                bcrypt.compare(req.body.password, data.password, (err, success) => {
+                    if (success)
+                        callback(null, data);
+                    else
+                        callback("password is incorrect", err);
+                })
+            } else callback(err, "user not in database");
         })
     } catch (e) {
         console.log(e);
@@ -21,9 +58,16 @@ exports.login = (req, callback) => {
 }
 exports.forgotPassword = (req, callback) => {
     try {
-        model.forgotPassword(req, (err, data) => {
-            if (err) callback(err)
-            else callback(null, data)
+        model.user.findOne({
+            email: req.body.email
+        }, (err, data) => {
+            if (data) {
+                callback(null, data)
+                console.log(data);
+            } else {
+                callback(err)
+                console.log("user not found!");
+            }
         })
     } catch (e) {
         console.log(e);
@@ -31,9 +75,25 @@ exports.forgotPassword = (req, callback) => {
 }
 exports.resetPassword = (req, callback) => {
     try {
-        model.resetPassword(req, (err, data) => {
-            if (err) callback(err)
-            else callback(null, data)
+        bcrypt.hash(req.body.password, 10, (err, encrypted) => {
+            if (err) {
+                callback(err)
+                console.log("error in salting!!");
+            } else {
+                model.user.updateOne({
+                    _id: req.decoded.payload
+                }, {
+                    password: encrypted
+                }, (err, data) => {
+                    if (err) {
+                        callback(err)
+                        console.log("error in updating password!!");
+                    } else {
+                        callback(null, data)
+                        console.log('updated password successfully!!', data);
+                    }
+                })
+            }
         })
     } catch (e) {
         console.log(e);
