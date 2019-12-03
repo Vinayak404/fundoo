c9ab50e9cc9e64cda812f999c9a1f6985cc8dc65
Vinayak <vinayakavastrd@gmail.com>
2019-12-03 18:28:04 +0530

    [Vinayak] add . added notes API's for addNotes,getNotes,controller/notesController.js

--- controller/notesController.js
+++ controller/notesController.js
@@ -5,7 +5,7 @@ exports.addNote = (req, res) => {
     let error = req.validationErrors();
     let response = {};
     if (error) {
-        response.succcess = false;
+        response.success = false;
         response.error = error;
         res.status(404).send(response)
     } else {
@@ -19,5 +19,76 @@ exports.addNote = (req, res) => {
                 response.error = err;
                 res.status(500).send(response)
             })
+    }
+}
+exports.getNotes = (req, res) => {
+    try {
+        let response = {}
+        notesServices.getNotes(req)
+            .then((data) => {
+                response.success = true;
+                response.data = data
+                res.status(200).send(response)
+            }).catch((err) => {
+                response.success = false;
+                response.error = err;
+                res.status(500).send(response)
+            })
+    } catch (e) {
+        console.log(e);
+
+    }
+}
+exports.deleteNote = (req, res) => {
+    try {
+        let response = {};
+        notesServices.deleteNote(req)
+
+            .then((data) => {
+                response.success = true;
+                response.data = data;
+                res.status(200).send(response);
+            }).catch((err) => {
+                response.success = false;
+                response.error = err;
+                res.status(500).send(response)
+            })
+    } catch (e) {
+        console.log(e);
+
+    }
+}
+exports.editTitle = (req, res) => {
+    try {
+        let response = {};
+        notesServices.editTitle(req)
+            .then((data) => {
+                response.success = true;
+                response.data = data;
+                res.status(200).send(response)
+            }).catch((err) => {
+                response.success = true;
+                response.err = err
+            })
+    } catch (e) {
+        console.log(e);
+    }
+}
+exports.editDescription = (req, res) => {
+    try {
+        let response = {};
+        notesServices.editDescription(req)
+            .then((data) => {
+                response.success = true;
+                response.data = data;
+                res.status(200).send(response);
+            }).catch((err) => {
+                response.success = false;
+                response.err = err;
+                res.status(500).send(response)
+            })
+    } catch (e) {
+        console.log(e);
+
     }
 }
--- router/notesRouter.js
+++ router/notesRouter.js
@@ -2,8 +2,5 @@ const notesController = require('../controller/notesController')
 const verifyToken = require('../middleware/token')
 const express = require('express');
 const notesRouter = express.Router();
-notesRouter.post('/notes/addNote', notesController.addNote);
-// router.post('/user/login', userController.login);
-// router.post('/user/forgotPassword', userController.forgotPassword);
-// router.post('/user/resetPassword/:token', verifyToken.verify, userController.resetPassword);
+notesRouter.post('/notes/addNote', verifyToken.userVerify, notesController.addNote);
 module.exports = notesRouter;
