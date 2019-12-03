b5de78dd110811a3bdc5b9c2abe28175f30cf796
Vinayak <vinayakavastrd@gmail.com>
2019-12-03 18:43:37 +0530

    [Vinayak] add . added notes API's for addNotes,getNotes,deleteNote,editNote services/notesServices.js

--- services/notesServices.js
+++ services/notesServices.js
@@ -9,6 +9,53 @@ exports.addNote = (req) => {
         note.save((err, data) => {
             if (err) reject(err)
             else resolve(data)
+        });
+    })
+}
+exports.getNotes = (req) => {
+    return new Promise((resolve, reject) => {
+        model.notesModel.find({
+            _userId: req.decoded.payload.id,
+            isDeleted: false
+        }, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
+}
+exports.deleteNote = (req) => {
+    return new Promise((resolve, reject) => {
+        model.notesModel.findByIdAndUpdate({
+            _id: req.body._id
+        }, {
+            isDeleted: true
+        }, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
+}
+exports.editTitle = (req) => {
+    return new Promise((resolve, reject) => {
+        model.notesModel.findByIdAndUpdate({
+            _id: req.body._id
+        }, {
+            title: req.body.title
+        }, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+        })
+    })
+}
+exports.editDescription = (req) => {
+    return new Promise((resolve, reject) => {
+        model.notesModel.findByIdAndUpdate({
+            _id: req.body._id
+        }, {
+            description: req.body.description
+        }, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
         })
     })
 }
