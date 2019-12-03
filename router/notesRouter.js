c7a9398ceb078250c73012434098a63271a0531a
Vinayak <vinayakavastrd@gmail.com>
2019-12-03 18:34:41 +0530

    [Vinayak] add . added notes API's for addNotes,getNotes,router/notesRouter.js

--- router/notesRouter.js
+++ router/notesRouter.js
@@ -1,6 +1,10 @@
-const notesController = require('../controller/notesController')
-const verifyToken = require('../middleware/token')
+const notesController = require('../controller/notesController');
+const verifyToken = require('../middleware/token');
 const express = require('express');
 const notesRouter = express.Router();
-notesRouter.post('/notes/addNote', verifyToken.userVerify, notesController.addNote);
+notesRouter.post('/addNote', verifyToken.userVerify, notesController.addNote);
+notesRouter.get('/getNotes', verifyToken.userVerify, notesController.getNotes);
+notesRouter.put('/deleteNote', notesController.deleteNote);
+notesRouter.put('/editTitle', notesController.editTitle);
+notesRouter.put('/editDescription', notesController.editDescription);controller/notesController.js
 module.exports = notesRouter;
