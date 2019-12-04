333da7d8ae97985bae9c988f444d4d0ef2919b21
Vinayak <vinayakavastrd@gmail.com>
2019-12-04 19:25:46 +0530

    [Vinayak] add. added imageUpload API using s3 services/fileUploadservices.js

---
+++ services/fileUploadServices.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const AWS = require('aws-sdk');
+const multer = require('multer');
+const multers3 = require('multer-s3');
+const accessKeyId = process.env.ACCESSID;
+const secretKey = process.env.SECRETKEY;
+const bucketName = process.env.BUCKETNAME;
+AWS.config.update({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretKey,
+    Bucket: bucketName
+})
+const s3 = new AWS.S3();
+const upload = multer({
+    storage: multers3({
+        s3: s3,
+        bucket: bucketName,
+        contentType: multers3.AUTO_CONTENT_TYPE,
+        acl: 'public-read',
+        metadata: (req, file, callback) => {
+            console.log("file in multer", file);
+            callback(null, {
+                fieldName: 'Profile'
+            })
+        },
+        key: (req, file, callback) => {
+            callback(null, Date.now().toString())
+        }
+    })
+})
+module.exports = upload
