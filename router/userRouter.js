3f05c6722e6f2597acb1594d429d4753fc2bdf17
Vinayak <vinayakavastrd@gmail.com>
2019-12-04 19:23:59 +0530

    [Vinayak] add. added imageUpload API using s3 router/userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,9 +1,21 @@
-const userController = require('../controller/userController')
-const verifyToken = require('../middleware/token')
+const userController = require('../controller/userController');
+const verifyToken = require('../middleware/token');
 const express = require('express');
 const router = express.Router();
+const upload = require('../services/fileUploadServices');
+const singleUpload = upload.single('image');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/forgotPassword', userController.forgotPassword);
 router.post('/resetPassword/:token', verifyToken.verify, userController.resetPassword);
+router.post('/uploadImage', verifyToken.userVerify, (req, res, next) => {
+    // console.log('req', req);
+    singleUpload(req, res, (err) => {
+        console.log("req.file", req.file);
+        const imageURL = req.file.location;
+        console.log("image URL", imageURL);
+        req.imageURL = imageURL;
+        next();
+    });
+}, userController.uploadImage);
 module.exports = router;
