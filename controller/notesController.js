c5bf12746690f516790c594be9616bac7a5a00c3
Vinayak <vinayakavastrd@gmail.com>
2019-12-09 19:13:15 +0530

    [Vinayak] add. added collaborator API for add collaborator controller/notesController.js

--- controller/notesController.js
+++ controller/notesController.js
@@ -1,5 +1,7 @@
 const notesServices = require('../services/notesServices')
 exports.addNote = (req, res) => {
+    console.log(req.decoded);
+
     req.checkBody('title', "Enter valid title").notEmpty();
     req.checkBody('description', "Enter valid description").notEmpty()
     let error = req.validationErrors();
@@ -71,6 +73,33 @@ exports.editNote = (req, res) => {
                 response.err = err;
                 res.status(500).send(response);
             })
+    } catch (e) {
+        console.log(e);
+    }
+}
+exports.collaborate = (req, res) => {
+    try {
+        req.checkBody('noteId', 'invalid model id').notEmpty();
+        req.checkBody('collabId', 'invalid collab id').notEmpty();
+        let error = req.validationErrors();
+        let response = {}
+        if (error) {
+            response.success = false;
+            response.err = error
+            res.status(400).send(response);
+        } else {
+            notesServices.collaborate(req)
+                .then((data) => {
+                    response.success = true;
+                    response.data = data;
+                    res.status(200).send(response);
+                }).catch((err) => {
+                    response.success = false;
+                    response.error = err;
+                    res.status(422).send(response)
+                })
+        }
+
     } catch (e) {
         console.log(e);
     }
