c8ee08b5d27158445ba1033ae2a0387f93481172
Vinayak <vinayakavastrd@gmail.com>
2019-12-09 19:15:09 +0530

    [Vinayak] add. added collaborator API for add collaborator router/userRouter.js

--- router/userRouter.js
+++ router/userRouter.js
@@ -2,9 +2,11 @@ const userController = require('../controller/userController');
 const verifyToken = require('../helpers/token')
 const express = require('express');
 const router = express.Router();
+const upload = require('../helpers/multer');
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/forgotPassword', userController.forgotPassword);
 router.post('/resetPassword', verifyToken.userVerify, userController.resetPassword);
-router.post('/uploadpic', verifyToken.userVerify, userController.uploadpic)
+// router.post('/uploadpic', verifyToken.userVerify, userController.uploadpic)
+router.post('/uploadpic', verifyToken.userVerify, upload.single('image'), userController.uploadpic)
 module.exports = router;
