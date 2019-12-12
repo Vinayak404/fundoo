cae7d5d332855d3387507bce0a1333316b6bb189
Vinayak <vinayakavastrd@gmail.com>
2019-12-12 19:15:35 +0530

    [Vinayak] refactor. comments added

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,3 +1,12 @@
+/********************************************************************************************************
+ * @Execution : default node : cmd> npm start
+ * @Purpose : backend of user login using express node.js for Fundoo app
+ * @description :using express framework and connect socket.
+ * @overview :API's for backend.
+ * @author : Vinayaka.S.V <vinayakavastrad@gmail.com> 
+ * @version : 1.0
+ * @since : 30/11/2019
+ *********************************************************************************************************/
 const userController = require('../controller/userController');
 const verifyToken = require('../helpers/token')
 const express = require('express');
