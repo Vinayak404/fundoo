d9df9b3e4765642979fbb5cb40a4f27342730f68
Vinayak <vinayakavastrd@gmail.com>
2019-12-14 18:55:34 +0530

    [Vinayak] add. started with elastic search

--- router/userRouter.js
+++ router/userRouter.js
@@ -8,7 +8,7 @@
  * @since : 30/11/2019
  *********************************************************************************************************/
 const userController = require('../controller/userController');
-const verifyToken = require('../helpers/token')
+const verifyToken = require('../helpers/token');
 const express = require('express');
 const router = express.Router();
 const upload = require('../helpers/multer');
@@ -16,5 +16,5 @@ router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/forgotPassword', userController.forgotPassword);
 router.post('/resetPassword', verifyToken.userVerify, userController.resetPassword);
-router.post('/uploadpic', verifyToken.userVerify, upload.single('image'), userController.uploadpic)
+router.post('/uploadpic', verifyToken.userVerify, upload.single('image'), userController.uploadpic);
 module.exports = router;
