71d41ddbc7dfd06ef16409c2a129e8f554be8fbb
Vinayak <vinayakavastrd@gmail.com>
2019-12-16 19:25:01 +0530

    [Vinayak] add. implemented elastic search for notes

--- helpers/elasticSearch.js
+++ helpers/elasticSearch.js
@@ -13,7 +13,72 @@ exports.createIndex = (req, res) => {
             console.log("result in index elastic search-->", data);
             res.status(200).send(data)
         } else {
+            res.status(422).send(err)
             console.log("error in index", err);
         }
     })
+}
+exports.addDocument = (data1) => {
+    try {
+        let bulk = []
+        console.log('in add doc!!');
+        data1.forEach(element => {
+            bulk.push({
+                index: {
+                    _index: element.userId,
+                    _type: "notes"
+                }
+            });
+            let data = {
+                "id": element._id,
+                "title": element.title,
+                "description": element.description,
+                "isDeleted": element.isDeleted,
+                "isArchived": element.isArchived
+            }
+            bulk.push(data)
+        });
+        client.bulk({
+            body: bulk
+        }, (err, data) => {
+            if (err) console.log("error in bulking");
+            else console.log("successfully bulked", data);
+
+        })
+
+    } catch (e) {
+        console.log(e);
+
+    }
+}
+  
+exports.search = (req, res) => {
+    try {
+        console.log(req.decoded.payload.id);
+
+        let userid = req.decoded.payload.id
+        let body = {
+            query: {
+                query_string: {
+                    query: `${req.body.key}`,
+                    analyze_wildcard: true,
+                    fields: ["title", "description", "isDeleted", "isArchived"]
+                }
+            }
+        }
+        client.search({
+                index: userid,
+                body: body,
+                type: "notes"
+            }).then((data) => {
+                console.log(data)
+                res.status(200).send(data)
+            })
+            .catch((err) => {
+                console.log(err)
+                res.status(404).send(err)
+            })
+    } catch (e) {
+        console.log(e);
+    }
 }
